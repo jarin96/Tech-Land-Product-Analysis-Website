3b1c4b548cf8ee25c2dfa080528806a3c1fe11a6
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-04-04 10:55:26 +0600

    Question & answer part done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -9,11 +9,36 @@ const Blogs = () => {
                 <Card.ImgOverlay>
                     <Card.Title>What is the purpose of context api?</Card.Title>
                     <Card.Text>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora obcaecati necessitatibus, debitis voluptatibus molestias ut asperiores, eius adipisci pariatur ipsa velit doloremque aspernatur nemo magni, rerum perferendis soluta ratione delectus! Laudantium, eaque unde. Consequuntur pariatur maiores aspernatur. Ipsa, cupiditate ex eligendi eum nostrum voluptates, corporis hic commodi doloribus consequatur magni ea vel architecto velit itaque? Id consectetur eligendi porro quod asperiores corporis, aliquam consequatur? Ex cum laborum, magnam est iure corrupti neque libero temporibus in error deleniti nihil totam dolorem ipsam quibusdam, sint quos quia, vero molestias placeat enim consectetur eum dicta soluta? Est culpa, itaque optio error provident maiores?
+                        Context api is a easy way to create global varialbles that can be pass around in the React app and this is an alternative to props drilling.
+
+                        Context api mainly take steps to pass data from the component tree without having to pass the same props from component to component down manually at every level.
+
+                        Context api is mainly used when the data needs to be pass by many components at different nesting levels.
                     </Card.Text>
                     <Card.Title>What is semantic tag?</Card.Title>
                     <Card.Text>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora obcaecati necessitatibus, debitis voluptatibus molestias ut asperiores, eius adipisci pariatur ipsa velit doloremque aspernatur nemo magni, rerum perferendis soluta ratione delectus! Laudantium, eaque unde. Consequuntur pariatur maiores aspernatur. Ipsa, cupiditate ex eligendi eum nostrum voluptates, corporis hic commodi doloribus consequatur magni ea vel architecto velit itaque? Id consectetur eligendi porro quod asperiores corporis, aliquam consequatur? Ex cum laborum, magnam est iure corrupti neque libero temporibus in error deleniti nihil totam dolorem ipsam quibusdam, sint quos quia, vero molestias placeat enim consectetur eum dicta soluta? Est culpa, itaque optio error provident maiores?
+                        Semantic tag is a one kind of HTML tags that provide information about the contents of these tags like Header ,
+                        that goes beyond on the page as it is.
+                        Semantic tags are mainly the main part of the body tags.
+                        By adding semantic tag to your web page you provide additional intelligence that helps google to understand the roles and importance of the different portion of your web page.
+                        Those are some of the semantic tags example Header,Footer,nav,article,section.
+                        When a page is designed, without semantic tag it is really difficult to identify the various parts of a web page.
+
+                        But when a page is designed with various semantic tags it is really easy to find the various parts of the page at a glance.
+                    </Card.Text>
+                    <Card.Title>What is the difference between Inline-block and Inline-block elements?</Card.Title>
+                    <Card.Text>
+                        Inline block treats like other inline elements but also allows the useages of block properties.Block takes as mush width as they needed and start with a new line.
+
+                        Inline block elements are same as inline elements but they can also use padding and margins added on all four sides.
+                        A block level elements takes full width of the page by default.
+                        Even if there is a page the block level elements each starts with a new line.
+
+                        We can set an block level element with an height property, and width property.The example for block level element are
+                        div, p, h1, ui, footer.
+
+                        A inline element can be placed side by side.And a inline element takes space as much as they needed.
+                        Like block elements we can not set height and weight property for inline element.
                     </Card.Text>
                 </Card.ImgOverlay>
             </Card>
